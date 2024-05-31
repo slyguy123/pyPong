f3d39ffa333effaac580451832ae4f579e372b75
slyguy123 <guy.schuleman@gmail.com>
2024-05-31 16:30:18 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+#!/user/bin/python3
+
+# a python version of the famous pong game
+# theSlyestGuy
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("pyPong v1")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+
+# Paddle B
+
+# Ball 
+
+
+# Main game loop
+while True:
+    wn.update()
+
